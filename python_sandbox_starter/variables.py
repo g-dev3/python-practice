a4e33a4e7b1b17da658dae52006a5476ed419818
G Dev3 <gdev3.js@gmail.com>
2024-02-23 08:37:18 +0530

    Python Series for django

---
+++ python_sandbox_starter/variables.py
@@ -0,0 +1,32 @@
+# A variable is a container for a value, which can be of various types
+
+'''
+This is a 
+multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+"""
+VARIABLE RULES:
+  - Variable names are case sensitive (name and NAME are different variables)
+  - Must start with a letter or an underscore
+  - Can have numbers but can not start with one
+"""
+x = 20
+
+y= 40.90
+
+x = str(x)
+y = int(y)
+print(type(x))
+print(type(y))
+print(y)
+x = float(x)
+
+print(x)
+x = int(x)
+print(type(x))
+# z = x+y
+
+# print(z)
